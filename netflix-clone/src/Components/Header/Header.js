dcfd06c8d658c3e6928e0211e6f9bf04dc3f7816
Hailemelekot <amanitinbite@gmail.com>
2024-09-22 12:59:47 +0200

    header and footer

--- netflix-clone/src/Components/Header/Header.js
+++ netflix-clone/src/Components/Header/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Header.css'
 import img1 from '../../assets/Img/image.png'
+import NotificationsSharpIcon from '@mui/icons-material/NotificationsNoneSharp';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountBoxSharpIcon from '@mui/icons-material/AccountBoxSharp';
+import ArrowDropDownSharpIcon from '@mui/icons-material/ArrowDropDownSharp';
+import '../../index.css';
 function Header() {
   return (
+    <section className='App'>
     <div className='header_outer_container'>
       <div className='header_container'>
         <div className='header_left'>
@@ -19,14 +25,15 @@ function Header() {
         </div>
         <div className='header_right'>
           <ul>
-            {/* <li><SearchIcon /></li>
-            <li><NoteficationsNoneIcons /></li>
-            <li><AccountBoxIcon /></li>
-            <li><ArrowDropDownIcon /></li> */}
+            <SearchIcon />
+            <NotificationsSharpIcon />
+            <AccountBoxSharpIcon />
+            <ArrowDropDownSharpIcon />
           </ul>
         </div>
       </div>
     </div>
+  </section>
   )
 }
 
